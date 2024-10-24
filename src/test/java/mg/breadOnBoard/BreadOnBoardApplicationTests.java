f50d99ab4c78cc325f24c1fa28f03c792ee77a95
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-24 13:15:52 +0300

    Initializing back-end project of bread on board

---
+++ src/test/java/mg/breadOnBoard/BreadOnBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.breadOnBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadOnBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
