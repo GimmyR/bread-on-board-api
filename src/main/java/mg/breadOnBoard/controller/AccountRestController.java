ac48151846fbb811a35c25ead8d9a958d6971867
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-11-27 14:09:07 +0300

    Removing Session Model, Repository and RestController

--- src/main/java/mg/breadOnBoard/controller/AccountRestController.java
+++ src/main/java/mg/breadOnBoard/controller/AccountRestController.java
@@ -10,12 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import mg.breadOnBoard.model.Account;
-import mg.breadOnBoard.model.Session;
-import mg.breadOnBoard.service.AccountNotFoundException;
 import mg.breadOnBoard.service.AccountService;
-import mg.breadOnBoard.service.SessionNotFoundException;
-import mg.breadOnBoard.service.SessionService;
 
 @RestController
 public class AccountRestController {
@@ -25,9 +20,6 @@ public class AccountRestController {
 	
 	@Autowired
 	private AccountService accountService;
-	
-	@Autowired
-	private SessionService sessionService;
 	
 	@PostMapping("/api/account/log-in")
 	public ResponseEntity<String> logIn(@RequestParam String username, @RequestParam String password) {
@@ -42,44 +34,6 @@ public class AccountRestController {
 		} catch(AuthenticationException e) {
 			
 			response = new ResponseEntity<String>("Connexion impossible", HttpStatus.UNAUTHORIZED);
-			
-		} return response;
-		
-	}
-	
-	@PostMapping("/api/account/auth")
-	public ResponseEntity<String> auth(@RequestParam String token) {
-		
-		ResponseEntity<String> response = null;
-		
-		try {
-			
-			Session session = sessionService.findById(token);
-			Account account = accountService.findById(session.getAccountId());
-			response = new ResponseEntity<String>(account.getUsername(), HttpStatus.OK);
-			
-		} catch (SessionNotFoundException | AccountNotFoundException e) {
-
-			response = new ResponseEntity<String>("Session introuvable !", HttpStatus.NOT_FOUND);
-			
-		} return response;
-		
-	}
-	
-	@PostMapping("/api/account/log-out")
-	public ResponseEntity<String> logOut(@RequestParam String token) {
-		
-		ResponseEntity<String> response = null;
-		
-		try {
-			
-			Session session = sessionService.findById(token);
-			sessionService.delete(session);
-			response = new ResponseEntity<String>("Vous êtes déconnecté !", HttpStatus.OK);
-			
-		} catch (SessionNotFoundException e) {
-
-			response = new ResponseEntity<String>("Session introuvable !", HttpStatus.NOT_FOUND);
 			
 		} return response;
 		
